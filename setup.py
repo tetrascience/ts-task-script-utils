7bbfed103384bee4d2ae86490602b3df1475ddbf
Kai Wang <wk.open@gmail.com>
2021-03-03 21:12:52 -0500

    Bumped to v1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "ts-task-script-utils"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
